2d5457102f40fadc156b062ff3894e5597dc9ad2
Chris Miller <lordsauronthegreat@gmail.com>
2011-03-10 15:26:58 -0700

    Mucho boredom creates documentation.

--- src/list.h
+++ src/list.h
@@ -18,6 +18,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* GENERIC/TEMPLATE LIST IMPLEMENTATION - IN C!
+ * List variant: doubly-linked non-circular
+ * Guiding principle: be as light and fast as possible without actually using
+ * assembler. As a result there is little if no sanity checking in this code.
+ * Usage: (better examples will be in the unit tests... when I write those).
+ *   #include "list.h"
+ *   #LIST_PROTOTYPE(intlist, int);
+ *   intlist_list * l = intlist_list_create();
+ *   l->data = 0; // [0]
+ *   intlist_list_insert(l, 1); // [0, 1]
+ *   intlist_list_free_all(l); // NULL
+ *   // and so on and so forth
+ * Generally speaking, if you're planning on using this, you already know how
+ * to make something like this, and you're just grabbing (and/or modifying) my
+ * code to prevent yourself from having to bother doing it yourself.
+ */
+
 #define LIST_PROTOTYPE(name, type)                                            \
 LIST_CPLUSPLUS_ESC_BEGIN                                                      \
 typedef struct __##name##_list __##name##_list;                               \
@@ -45,6 +62,7 @@ void name##_list_free_all(name##_list * l) {                                  \
     }                                                                         \
     free(cursor->data);                                                       \
     free(cursor);                                                             \
+    l = NULL;                                                                 \
 }                                                                             \
 static inline /* inserts a new element after the given element */             \
 void name##_list_insert(name##_list * at,                                     \
@@ -53,7 +71,8 @@ void name##_list_insert(name##_list * at,                                     \
     n_elem->data = elem;                                                      \
     n_elem->next = at->next;                                                  \
     n_elem->prev = at;                                                        \
-    at->next->prev = n_elem;                                                  \
+    if(at->next != NULL)                                                      \
+        at->next->prev = n_elem;                                              \
     at->next = n_elem;                                                        \
 } /* cut elements from the list beginning at start and ending at end */       \
 static inline /* this does not check for sanity for you to optimize speed */  \
@@ -64,8 +83,4 @@ name##_list name##_list_remove(name##_list * start, /* if it doesn't work, */ \
     start_anc->next = end_child; end_child->prev = start_anc;                 \
     return start; /* returns the removed list */                              \
 }
-    
-                               
-    
-    
 
